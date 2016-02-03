936b748fc3fa5e0577e49218b7f5d90aea7d0e8d
showwin <showwin_kmc@yahoo.co.jp>
2016-02-03 20:47:55 +0900

    Add adapting the other DOM

--- yh_scraper.rb
+++ yh_scraper.rb
@@ -15,12 +15,21 @@ class YHScraper
     elms = doc.xpath(UPDATE_HISTORY_TABLE_XPATH)
 
     # 過去2回分の更新を取得
-    @date1 = elms[0].xpath('./td').first.content
-    @text1 = elms[1].xpath('./td')[1].content
-    @date2 = elms[3].xpath('./td').first.content
-    @text2 = elms[4].xpath('./td')[1].content
-  rescue
-    @cannot_scrape = true
+    begin
+      @date1 = elms[0].xpath('./td')[0].content
+      @text1 = elms[1].xpath('./td')[1].content
+      @date2 = elms[3].xpath('./td')[0].content
+      @text2 = elms[4].xpath('./td')[1].content
+    rescue
+      begin
+        @date1 = elms[1].xpath('./td')[0].content
+        @text1 = elms[2].xpath('./td')[1].content
+        @date2 = elms[4].xpath('./td')[0].content
+        @text2 = elms[5].xpath('./td')[1].content
+      rescue
+        @cannot_scrape = true
+      end
+    end
   end
 
   def dom_changed?
